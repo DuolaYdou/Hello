0e37071c1729de5a603c37b31022347384058b54
Ydou <3419812035@qq.com>
2017-12-29 21:29:26 +0800

    1

---
+++ app/src/main/java/com/example/cuihaojie/hello/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cuihaojie.hello
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val a:Int=1
+        val b=1
+
+    }
+}
